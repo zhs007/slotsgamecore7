c832fa0e4280fc6e41282c94cacbf50ce9991ab8
zhs007 <sssxueren@gmail.com>
2020-08-18 00:22:31 +0800

    upd

--- utils/time.go
+++ utils/time.go
@@ -6,23 +6,39 @@ import "time"
 type TimeI interface {
 	// Now - get now time
 	Now() time.Time
+	// GetDefaultLocation - get DefaultLocation
+	GetDefaultLocation() *time.Location
 }
 
 // Time - default Time
-type Time struct{}
+type Time struct {
+	local *time.Location
+}
 
 // Now - get now time
 func (t Time) Now() time.Time {
 	return time.Now()
 }
 
+// GetDefaultLocation - get DefaultLocation
+func (t Time) GetDefaultLocation() *time.Location {
+	return t.local
+}
+
 var gTime TimeI
 
 // FormatNow - format time
 func FormatNow(t TimeI) string {
-	return t.Now().Format("2006-01-02_15:04:05")
+	return t.Now().In(t.GetDefaultLocation()).Format("2006-01-02_15:04:05")
 }
 
 func init() {
-	gTime = &Time{}
+	t := &Time{}
+
+	l, err := time.LoadLocation("")
+	if err == nil {
+		t.local = l
+	}
+
+	gTime = t
 }
