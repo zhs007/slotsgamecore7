d99af6bb0f6c6ec43eff25d966f7d6ac817d4494
zhs007 <sssxueren@gmail.com>
2020-08-21 13:47:38 +0800

    Added Result

---
+++ game/result.go
@@ -0,0 +1,22 @@
+package sgc7game
+
+// ResultType - result type
+type ResultType int
+
+const (
+	// RTScatter - scatter
+	RTScatter = 1
+	// RTLine - line
+	RTLine = 2
+)
+
+// Result - result for slots game
+type Result struct {
+	Type      ResultType
+	LineIndex int
+	Symbol    int
+	Mul       int
+	Win       int
+	RealWin   int
+	Data      string
+}
