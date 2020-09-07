23c1c2abcf447f49fc144dd7f38fbd846c1f7ee6
zhs007 <sssxueren@gmail.com>
2020-09-07 17:32:29 +0800

    - Added NextGameModParams into sgc7game.PlayResult

--- game/playresult.go
+++ game/playresult.go
@@ -1,15 +1,52 @@
 package sgc7game
 
+import (
+	jsoniter "github.com/json-iterator/go"
+	sgc7utils "github.com/zhs007/slotsgamecore7/utils"
+	"go.uber.org/zap"
+)
+
 // PlayResult - result for play
 type PlayResult struct {
-	CurGameMod    string       `json:"curgamemod"`
-	NextGameMod   string       `json:"nextgamemod"`
-	Scenes        []*GameScene `json:"scenes"`
-	Results       []*Result    `json:"results"`
-	NextCmds      []string     `json:"-"`
-	NextCmdParams []string     `json:"-"`
-	CoinWin       int          `json:"-"`
-	CashWin       int64        `json:"-"`
-	IsFinish      bool         `json:"-"`
-	IsWait        bool         `json:"-"`
+	CurGameMod        string       `json:"curgamemod"`
+	NextGameMod       string       `json:"nextgamemod"`
+	NextGameModParams interface{}  `json:"nextgamemodparams"`
+	Scenes            []*GameScene `json:"scenes"`
+	Results           []*Result    `json:"results"`
+	NextCmds          []string     `json:"-"`
+	NextCmdParams     []string     `json:"-"`
+	CoinWin           int          `json:"-"`
+	CashWin           int64        `json:"-"`
+	IsFinish          bool         `json:"-"`
+	IsWait            bool         `json:"-"`
+}
+
+// PlayResult2JSON - PlayResult => json
+func PlayResult2JSON(pr *PlayResult) ([]byte, error) {
+	json := jsoniter.ConfigCompatibleWithStandardLibrary
+
+	b, err := json.Marshal(pr)
+	if err != nil {
+		sgc7utils.Warn("sgc7game.PlayResult2JSON",
+			zap.Error(err))
+
+		return nil, err
+	}
+
+	return b, nil
+}
+
+// JSON2PlayResult - json => PlayResult
+func JSON2PlayResult(buf []byte, pr *PlayResult) (*PlayResult, error) {
+	json := jsoniter.ConfigCompatibleWithStandardLibrary
+
+	// pr := &PlayResult{}
+	err := json.Unmarshal(buf, &pr)
+	if err != nil {
+		sgc7utils.Warn("sgc7game.JSON2PlayResult",
+			zap.Error(err))
+		return nil, err
+	}
+
+	return pr, nil
 }
---
+++ game/playresult_test.go
@@ -0,0 +1,44 @@
+package sgc7game
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_NextGameModParams(t *testing.T) {
+	type TestParams struct {
+		A int     `json:"a"`
+		B string  `json:"b"`
+		C float32 `json:"c"`
+		D []int   `json:"d"`
+	}
+
+	tp := &TestParams{A: 123, B: "456", C: 7.89, D: []int{10, 11, 12}}
+	pr := &PlayResult{
+		NextGameModParams: tp,
+	}
+
+	buf, err := PlayResult2JSON(pr)
+	assert.NoError(t, err)
+	t.Logf(string(buf))
+
+	pr2 := &PlayResult{
+		NextGameModParams: &TestParams{},
+	}
+	pr1, err := JSON2PlayResult(buf, pr2)
+	assert.NoError(t, err)
+
+	tp1, isok := pr1.NextGameModParams.(*TestParams)
+	assert.Equal(t, isok, true)
+	assert.NotNil(t, tp1)
+
+	assert.Equal(t, tp1.A, 123)
+	assert.Equal(t, tp1.B, "456")
+	assert.Equal(t, tp1.C, float32(7.89))
+	assert.Equal(t, tp1.D[0], 10)
+	assert.Equal(t, tp1.D[1], 11)
+	assert.Equal(t, tp1.D[2], 12)
+
+	t.Logf("Test_NextGameModParams OK")
+}
