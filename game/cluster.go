38c841553ab789c8ce01ef6652c074a4a686d860
zhs007 <sssxueren@gmail.com>
2021-07-29 10:00:03 +0000

    fixed a bug in cluster

--- game/cluster.go
+++ game/cluster.go
@@ -63,27 +63,27 @@ func calcClusterSymbol(scene *GameScene, x, y int, symbol int, pos []int,
 				pos = calcClusterSymbol(scene, x-1, y-1, symbol, pos, isSameSymbol)
 			}
 
-			pos = calcClusterSymbol(scene, x-1, y, symbol, pos, isSameSymbol)
+			// pos = calcClusterSymbol(scene, x-1, y, symbol, pos, isSameSymbol)
 
 			if y < scene.Height-1 {
 				pos = calcClusterSymbol(scene, x-1, y+1, symbol, pos, isSameSymbol)
 			}
 		}
 
-		if y > 0 {
-			pos = calcClusterSymbol(scene, x, y-1, symbol, pos, isSameSymbol)
-		}
+		// if y > 0 {
+		// 	pos = calcClusterSymbol(scene, x, y-1, symbol, pos, isSameSymbol)
+		// }
 
-		if y < scene.Height-1 {
-			pos = calcClusterSymbol(scene, x, y+1, symbol, pos, isSameSymbol)
-		}
+		// if y < scene.Height-1 {
+		// 	pos = calcClusterSymbol(scene, x, y+1, symbol, pos, isSameSymbol)
+		// }
 
 		if x < scene.Width-1 {
 			if y > 0 {
 				pos = calcClusterSymbol(scene, x+1, y-1, symbol, pos, isSameSymbol)
 			}
 
-			pos = calcClusterSymbol(scene, x+1, y, symbol, pos, isSameSymbol)
+			// pos = calcClusterSymbol(scene, x+1, y, symbol, pos, isSameSymbol)
 
 			if y < scene.Height-1 {
 				pos = calcClusterSymbol(scene, x+1, y+1, symbol, pos, isSameSymbol)
