50ba8a9ea94f7c61acbb69a3447048e531e05625
zhs007 <sssxueren@gmail.com>
2022-03-10 07:29:45 +0000

    Fixed some bugs in rtp

--- rtp/worker.go
+++ rtp/worker.go
@@ -104,7 +104,7 @@ func StartRTP(game sgc7game.IGame, rtp *RTP, worknums int, spinnums int64, stake
 					i--
 				} else {
 					if limitPayout > 0 {
-						cp := 0
+						cp := int64(0)
 						for _, v := range results {
 							if cp+v.CashWin > limitPayout {
 								cp += v.CashWin
--- ver/version.go
+++ ver/version.go
@@ -1,4 +1,4 @@
 package sgc7ver
 
 // Version -
-const Version = "v0.9.33"
+const Version = "v0.9.35"
