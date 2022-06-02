ed58375f8f27b45dd28dc592b7adeda09f7d3361
zhs007 <sssxueren@gmail.com>
2022-06-02 05:21:14 +0000

    Updated relaxutils

--- relaxutils/config.go
+++ relaxutils/config.go
@@ -8,13 +8,72 @@ import (
 	"go.uber.org/zap"
 )
 
-type Config struct {
-	XMLName xml.Name `xml:"config"`
-	General string   `xml:",comment"`
-	SD      float32  `xml:"sd"`
+type FloatList struct {
+	Vals []float32 `xml:"item"`
 }
 
-func SaveConfig(fn string, cfg *Config) error {
+type StringList struct {
+	Vals []string `xml:"item"`
+}
+
+type Symbol struct {
+	XMLName xml.Name `xml:"symbol"`
+	Name    string   `xml:"name,attr"`
+	Val     int      `xml:"value,attr"`
+}
+
+type Symbols struct {
+	XMLName xml.Name  `xml:"symbols"`
+	Symbols []*Symbol `xml:"symbol"`
+}
+
+type PayoutWin struct {
+	XMLName xml.Name `xml:"win"`
+	Count   int      `xml:"count,attr"`
+	Payout  int      `xml:"payout,attr"`
+}
+
+type Payout struct {
+	XMLName xml.Name `xml:"symbol"`
+	Name    string   `xml:"name,attr"`
+	Wins    []*PayoutWin
+}
+
+type Payouts struct {
+	XMLName xml.Name `xml:"payouts"`
+	Payouts []*Payout
+}
+
+type Table struct {
+	XMLName      xml.Name      `xml:"table"`
+	TableComment string        `xml:",comment"`
+	Reel         []*StringList `xml:"reel"`
+}
+
+type Reels struct {
+	Tables []*Table
+}
+
+type Config struct {
+	XMLName            xml.Name    `xml:"config"`
+	GeneralComment     string      `xml:",comment"`
+	SD                 float32     `xml:"sd"`
+	RTP                float32     `xml:"rtp"`
+	ID                 int         `xml:"id"`
+	Name               string      `xml:"name"`
+	ConfigVersion      string      `xml:"configVersion"`
+	RTPsComment        string      `xml:",comment"`
+	RTPs               *FloatList  `xml:"rtps"`
+	ConfigComment      string      `xml:",comment"`
+	Symbols            *Symbols    `xml:"symbols"`
+	Wilds              *StringList `xml:"wilds"`
+	CreditsPerBet      int         `xml:"CREDITS_PER_BET"`
+	FSAwarded          int         `xml:"FS_AWARDED"`
+	FSAwardedRetrigger int         `xml:"FS_AWARDED_RETRIGGER"`
+	Payouts            *Payouts    `xml:"payouts"`
+}
+
+func SaveConfig(fn string, cfg interface{}) error {
 	output, err := xml.MarshalIndent(cfg, "  ", "    ")
 	if err != nil {
 		goutils.Error("SaveConfig:MarshalIndent",
--- relaxutils/config_test.go
+++ relaxutils/config_test.go
@@ -6,13 +6,113 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type TestConfig struct {
+	Config
+	BaseReels *Reels `xml:"basereels"`
+}
+
 func Test_Config(t *testing.T) {
-	cfg := &Config{
-		General: "General game information",
-		SD:      1.56,
+	cfg := Config{
+		GeneralComment: "General game information",
+		SD:             1.56,
+		RTP:            0.9607,
+		ID:             123,
+		Name:           "game001",
+		ConfigVersion:  "1.0",
+		RTPsComment:    "RTP infomation",
+		RTPs: &FloatList{
+			Vals: []float32{0.9607, 0.9608, 0.9609},
+		},
+		ConfigComment: "Configuration",
+		Symbols: &Symbols{
+			Symbols: []*Symbol{
+				{
+					Name: "A",
+					Val:  1,
+				},
+				{
+					Name: "B",
+					Val:  2,
+				},
+			},
+		},
+		Wilds: &StringList{
+			Vals: []string{"WL"},
+		},
+		CreditsPerBet:      25,
+		FSAwarded:          15,
+		FSAwardedRetrigger: 5,
+		Payouts: &Payouts{
+			Payouts: []*Payout{
+				{
+					Name: "A",
+					Wins: []*PayoutWin{
+						{
+							Count:  3,
+							Payout: 100,
+						},
+						{
+							Count:  4,
+							Payout: 200,
+						},
+						{
+							Count:  5,
+							Payout: 300,
+						},
+					},
+				},
+				{
+					Name: "B",
+					Wins: []*PayoutWin{
+						{
+							Count:  3,
+							Payout: 150,
+						},
+						{
+							Count:  4,
+							Payout: 250,
+						},
+						{
+							Count:  5,
+							Payout: 350,
+						},
+					},
+				},
+			},
+		},
 	}
 
-	err := SaveConfig("../unittestdata/relaxconfig.xml", cfg)
+	tcfg := &TestConfig{
+		cfg,
+		&Reels{
+			Tables: []*Table{
+				{
+					TableComment: "table 0",
+					Reel: []*StringList{
+						{
+							Vals: []string{"A", "B", "A"},
+						},
+						{
+							Vals: []string{"B", "B", "A"},
+						},
+					},
+				},
+				{
+					TableComment: "table 1",
+					Reel: []*StringList{
+						{
+							Vals: []string{"A", "B", "A"},
+						},
+						{
+							Vals: []string{"B", "B", "A"},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	err := SaveConfig("../unittestdata/relaxconfig.xml", tcfg)
 	assert.NoError(t, err)
 
 	t.Logf("Test_CalcScatter OK")
