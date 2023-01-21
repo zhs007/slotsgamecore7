e90e143c251408bfd5090fb2b60499862944a84c
zhs007 <sssxueren@gmail.com>
2023-01-21 11:01:53 +0000

    Added StrValWeights

---
+++ game/strvalweights.go
@@ -0,0 +1,165 @@
+package sgc7game
+
+import (
+	"strings"
+
+	"github.com/xuri/excelize/v2"
+	"github.com/zhs007/goutils"
+	sgc7plugin "github.com/zhs007/slotsgamecore7/plugin"
+	"go.uber.org/zap"
+)
+
+// StrValWeights
+type StrValWeights struct {
+	Vals      []string
+	Weights   []int
+	MaxWeight int
+}
+
+func NewStrValWeights(vals []string, weights []int) (*StrValWeights, error) {
+	if len(vals) != len(weights) {
+		goutils.Error("NewStrValWeights",
+			zap.Int("vals", len(vals)),
+			zap.Int("weights", len(weights)),
+			zap.Error(ErrInvalidValWeights))
+
+		return nil, ErrInvalidValWeights
+	}
+
+	vw := &StrValWeights{
+		Vals:      make([]string, len(vals)),
+		Weights:   make([]int, len(vals)),
+		MaxWeight: 0,
+	}
+
+	copy(vw.Vals, vals)
+	copy(vw.Weights, weights)
+
+	for _, v := range weights {
+		vw.MaxWeight += v
+	}
+
+	return vw, nil
+}
+
+func (vw *StrValWeights) Clone() *StrValWeights {
+	nvw := &StrValWeights{
+		Vals:      make([]string, len(vw.Vals)),
+		Weights:   make([]int, len(vw.Weights)),
+		MaxWeight: vw.MaxWeight,
+	}
+
+	copy(nvw.Vals, vw.Vals)
+	copy(nvw.Weights, vw.Weights)
+
+	return nvw
+}
+
+func (vw *StrValWeights) RandVal(plugin sgc7plugin.IPlugin) (string, error) {
+	if len(vw.Vals) == 1 {
+		return vw.Vals[0], nil
+	}
+
+	ci, err := RandWithWeights(plugin, vw.MaxWeight, vw.Weights)
+	if err != nil {
+		goutils.Error("StrValWeights.RandVal:RandWithWeights",
+			zap.Error(err))
+
+		return "", err
+	}
+
+	return vw.Vals[ci], nil
+}
+
+// CloneExcludeVal - clone & exclude a val
+func (vw *StrValWeights) CloneExcludeVal(val string) (*StrValWeights, error) {
+	if len(vw.Vals) <= 1 {
+		goutils.Error("StrValWeights.RandVal:CloneExcludeVal",
+			zap.Error(ErrInvalidValWeights))
+
+		return nil, ErrInvalidValWeights
+	}
+
+	nvw := vw.Clone()
+
+	for i, v := range vw.Vals {
+		if v == val {
+			nvw.Vals = append(nvw.Vals[0:i], nvw.Vals[i+1:]...)
+			nvw.Weights = append(nvw.Weights[0:i], nvw.Weights[i+1:]...)
+			nvw.MaxWeight -= vw.Weights[i]
+
+			return nvw, nil
+		}
+	}
+
+	goutils.Error("StrValWeights.RandVal:CloneExcludeVal",
+		zap.Error(ErrInvalidValWeightsVal))
+
+	return nil, ErrInvalidValWeightsVal
+}
+
+// LoadStrValWeightsFromExcel - load xlsx file
+func LoadStrValWeightsFromExcel(fn string) (*StrValWeights, error) {
+	f, err := excelize.OpenFile(fn)
+	if err != nil {
+		goutils.Error("LoadStrValWeightsFromExcel:OpenFile",
+			zap.String("fn", fn),
+			zap.Error(err))
+
+		return nil, err
+	}
+
+	lstname := f.GetSheetList()
+	if len(lstname) <= 0 {
+		goutils.Error("LoadStrValWeightsFromExcel:GetSheetList",
+			goutils.JSON("SheetList", lstname),
+			zap.String("fn", fn),
+			zap.Error(ErrInvalidReelsExcelFile))
+
+		return nil, ErrInvalidReelsExcelFile
+	}
+
+	rows, err := f.GetRows(lstname[0])
+	if err != nil {
+		goutils.Error("LoadStrValWeightsFromExcel:GetRows",
+			zap.String("fn", fn),
+			zap.Error(err))
+
+		return nil, err
+	}
+
+	mapcolname := make(map[int]string)
+
+	vals := []string{}
+	weights := []int{}
+
+	for y, row := range rows {
+		if y == 0 {
+			for x, colCell := range row {
+				mapcolname[x] = strings.ToLower(colCell)
+			}
+		} else {
+			for x, colCell := range row {
+				colname, isok := mapcolname[x]
+				if isok {
+					if colname == "val" {
+						vals = append(vals, colCell)
+					} else if colname == "weight" {
+						v, err := goutils.String2Int64(colCell)
+						if err != nil {
+							goutils.Error("LoadStrValWeightsFromExcel:String2Int64",
+								zap.String("weight", colCell),
+								zap.Error(err))
+
+							return nil, err
+						}
+
+						weights = append(weights, int(v))
+					}
+				}
+			}
+		}
+	}
+
+	return NewStrValWeights(vals, weights)
+}
