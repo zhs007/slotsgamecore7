dcd0f465cedd6b942819553b64f60455c8a5d802
zhs007 <sssxueren@gmail.com>
2023-02-03 17:26:42 +0800

    Added GenReelsMainSymbolsDistance

--- mathtoolset/genreels.go
+++ mathtoolset/genreels.go
@@ -1,8 +1,6 @@
 package mathtoolset
 
 import (
-	"context"
-
 	"github.com/zhs007/goutils"
 	sgc7plugin "github.com/zhs007/slotsgamecore7/plugin"
 	"go.uber.org/zap"
@@ -16,9 +14,9 @@ func genReel(plugin sgc7plugin.IPlugin, rs *ReelStats, minoff int) ([]int, error
 	excsym := []SymbolType{}
 
 	for nrs.TotalSymbolNum > 0 {
-		syms := nrs.BuildSymbols(excsym)
-		if len(syms) <= 0 {
-			goutils.Error("genReel:BuildSymbols",
+		syms, err := nrs.BuildSymbolsWithWeights(excsym)
+		if err != nil || syms.MaxWeight <= 0 {
+			goutils.Error("genReel:BuildSymbolsWithWeights",
 				goutils.JSON("excludeSymbols", excsym),
 				zap.Int("lastnum", nrs.TotalSymbolNum),
 				zap.Error(ErrNoValidSymbols))
@@ -26,7 +24,8 @@ func genReel(plugin sgc7plugin.IPlugin, rs *ReelStats, minoff int) ([]int, error
 			return nil, ErrNoValidSymbols
 		}
 
-		ci, err := plugin.Random(context.Background(), len(syms))
+		// ci, err := plugin.Random(context.Background(), len(syms))
+		s, err := syms.RandVal(plugin)
 		if err != nil {
 			goutils.Error("GenReels:Random",
 				zap.Error(err))
@@ -34,22 +33,23 @@ func genReel(plugin sgc7plugin.IPlugin, rs *ReelStats, minoff int) ([]int, error
 			return nil, err
 		}
 
-		s := syms[ci]
+		// s := syms[ci]
 
-		nrs.RemoveSymbol(s, 1)
+		nrs.RemoveSymbol(SymbolType(s), 1)
 
-		reel = append(reel, int(s))
+		reel = append(reel, s)
 
 		if len(excsym) >= minoff {
 			excsym = excsym[1:]
 		}
 
-		excsym = append(excsym, s)
+		excsym = append(excsym, SymbolType(s))
 	}
 
 	return reel, nil
 }
 
+// 纯随机，重复符号需要不重复
 func GenReels(rss *ReelsStats, minoff int, trytimes int) (*sgc7game.ReelsData, error) {
 	reels := sgc7game.NewReelsData(len(rss.Reels))
 
@@ -60,6 +60,94 @@ func GenReels(rss *ReelsStats, minoff int, trytimes int) (*sgc7game.ReelsData, e
 			reel, err := genReel(plugin, rs, minoff)
 			if err != nil {
 				goutils.Error("GenReels:genReel",
+					zap.Error(err))
+			} else {
+				reels.SetReel(i, reel)
+
+				break
+			}
+		}
+	}
+
+	return reels, nil
+}
+
+// 随机，主要符号需要尽量分散开
+func genReelsMainSymbolsDistance(plugin sgc7plugin.IPlugin, rs *ReelStats, mainSymbols []SymbolType, minoff int) ([]int, error) {
+	nrs := rs.Clone()
+	reel := []int{}
+	excsym := []SymbolType{}
+
+	msn := rs.CountSymbolsNum(mainSymbols)
+	msoff := rs.TotalSymbolNum / msn
+	curi := 0
+
+	for nrs.TotalSymbolNum > 0 {
+		var syms *sgc7game.ValWeights
+		var err error
+
+		if curi == msoff-1 {
+			curi = 0
+
+			syms, err = nrs.BuildSymbolsWithWeightsEx(mainSymbols)
+			if err != nil || syms.MaxWeight <= 0 {
+				syms, err = nrs.BuildSymbolsWithWeights2(excsym, mainSymbols)
+				if err != nil || syms.MaxWeight <= 0 {
+					goutils.Error("genReelsMainSymbolsDistance:BuildSymbols",
+						goutils.JSON("excludeSymbols", excsym),
+						zap.Int("lastnum", nrs.TotalSymbolNum),
+						zap.Error(ErrNoValidSymbols))
+
+					return nil, ErrNoValidSymbols
+				}
+			}
+		} else {
+			curi++
+
+			syms, err = nrs.BuildSymbolsWithWeights2(excsym, mainSymbols)
+			if err != nil || syms.MaxWeight <= 0 {
+				goutils.Error("genReelsMainSymbolsDistance:BuildSymbols",
+					goutils.JSON("excludeSymbols", excsym),
+					zap.Int("lastnum", nrs.TotalSymbolNum),
+					zap.Error(ErrNoValidSymbols))
+
+				return nil, ErrNoValidSymbols
+			}
+		}
+
+		s, err := syms.RandVal(plugin)
+		if err != nil {
+			goutils.Error("genReelsMainSymbolsDistance:Random",
+				zap.Error(err))
+
+			return nil, err
+		}
+
+		nrs.RemoveSymbol(SymbolType(s), 1)
+
+		reel = append(reel, s)
+
+		if len(excsym) >= minoff {
+			excsym = excsym[1:]
+		}
+
+		excsym = append(excsym, SymbolType(s))
+	}
+
+	return reel, nil
+}
+
+// 随机，主要符号需要尽量分散开
+func GenReelsMainSymbolsDistance(rss *ReelsStats, mainSymbols []SymbolType, minoff int, trytimes int) (*sgc7game.ReelsData, error) {
+	reels := sgc7game.NewReelsData(len(rss.Reels))
+
+	plugin := sgc7plugin.NewBasicPlugin()
+
+	for i, rs := range rss.Reels {
+		for j := 0; j < trytimes; j++ {
+			reel, err := genReelsMainSymbolsDistance(plugin, rs, mainSymbols, minoff)
+			if err != nil {
+				goutils.Error("GenReelsMainSymbolsDistance:genReelsMainSymbolsDistance",
 					zap.Error(err))
 			} else {
 				reels.SetReel(i, reel)
--- mathtoolset/genreels_test.go
+++ mathtoolset/genreels_test.go
@@ -26,3 +26,24 @@ func Test_GenReels(t *testing.T) {
 
 	t.Logf("Test_GenReels OK")
 }
+
+func Test_GenReelsMainSymbolsDistance(t *testing.T) {
+	rss, err := LoadReelsStats("../unittestdata/genreelssrc.xlsx")
+	assert.NoError(t, err)
+	assert.NotNil(t, rss)
+
+	assert.Equal(t, len(rss.Reels), 5)
+	assert.Equal(t, len(rss.Reels[0].MapSymbols), 13)
+	assert.Equal(t, len(rss.Reels[1].MapSymbols), 13)
+	assert.Equal(t, len(rss.Reels[2].MapSymbols), 13)
+	assert.Equal(t, len(rss.Reels[3].MapSymbols), 13)
+	assert.Equal(t, len(rss.Reels[4].MapSymbols), 13)
+
+	rd, err := GenReelsMainSymbolsDistance(rss, []SymbolType{0, 1, 2, 3}, 2, 100)
+	assert.NoError(t, err)
+	assert.NotNil(t, rd)
+
+	rd.SaveExcel("../unittestdata/genreelsmainsymbolsdistance.xlsx")
+
+	t.Logf("Test_GenReels OK")
+}
