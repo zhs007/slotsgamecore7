d9b9039839c840a65dce1f8f9d2e3d7d2a5733e6
zhs007 <sssxueren@gmail.com>
2023-02-19 16:14:17 +0800

    Updated stats.Reels

--- stats/symbolrtp.go
+++ stats/symbolrtp.go
@@ -1,6 +1,11 @@
 package stats
 
 import (
+	"fmt"
+	"sort"
+
+	"github.com/xuri/excelize/v2"
+	"github.com/zhs007/goutils"
 	sgc7game "github.com/zhs007/slotsgamecore7/game"
 	"github.com/zhs007/slotsgamecore7/mathtoolset"
 )
@@ -20,16 +25,27 @@ func (srtp *SymbolRTP) CalcRTP(totalBets int64, num int) float64 {
 	return float64(srtp.Wins[num-1]) / float64(totalBets)
 }
 
-func NewSymbolRTP(s mathtoolset.SymbolType, maxSymbolNum int) *SymbolRTP {
+func NewSymbolRTP(s mathtoolset.SymbolType, maxSymbolWinNum int) *SymbolRTP {
 	return &SymbolRTP{
 		Symbol: s,
-		Wins:   make([]int64, maxSymbolNum),
+		Wins:   make([]int64, maxSymbolWinNum),
 	}
 }
 
 type SymbolsRTP struct {
-	MapSymbols map[mathtoolset.SymbolType]*SymbolRTP
-	TotalBets  int64
+	MapSymbols      map[mathtoolset.SymbolType]*SymbolRTP
+	TotalBets       int64
+	MaxSymbolWinNum int
+}
+
+func (ssrtp *SymbolsRTP) GenSymbols() []mathtoolset.SymbolType {
+	symbols := []mathtoolset.SymbolType{}
+
+	for k := range ssrtp.MapSymbols {
+		symbols = append(symbols, k)
+	}
+
+	return symbols
 }
 
 func (ssrtp *SymbolsRTP) OnBet(bet int64) {
@@ -40,13 +56,41 @@ func (ssrtp *SymbolsRTP) OnWin(win *sgc7game.Result) {
 	ssrtp.MapSymbols[mathtoolset.SymbolType(win.Symbol)].OnWin(win)
 }
 
-func NewSymbolsRTP(maxSymbolNum int, lst []mathtoolset.SymbolType) *SymbolsRTP {
+func (ssrtp *SymbolsRTP) SaveSheet(f *excelize.File, sheet string) error {
+	f.SetCellValue(sheet, goutils.Pos2Cell(0, 0), "symbol")
+
+	symbols := ssrtp.GenSymbols()
+
+	for i := 0; i < ssrtp.MaxSymbolWinNum; i++ {
+		f.SetCellValue(sheet, goutils.Pos2Cell(i+1, 0), fmt.Sprintf("X%v", i+1))
+	}
+
+	sort.Slice(symbols, func(i, j int) bool {
+		return symbols[i] < symbols[j]
+	})
+
+	y := 1
+	for _, s := range symbols {
+		f.SetCellValue(sheet, goutils.Pos2Cell(0, y), s)
+
+		for i := 0; i < ssrtp.MaxSymbolWinNum; i++ {
+			f.SetCellValue(sheet, goutils.Pos2Cell(i+1, y), ssrtp.MapSymbols[s].CalcRTP(ssrtp.TotalBets, i+1))
+		}
+
+		y++
+	}
+
+	return nil
+}
+
+func NewSymbolsRTP(maxSymbolWinNum int, lst []mathtoolset.SymbolType) *SymbolsRTP {
 	ssrtp := &SymbolsRTP{
-		MapSymbols: make(map[mathtoolset.SymbolType]*SymbolRTP),
+		MapSymbols:      make(map[mathtoolset.SymbolType]*SymbolRTP),
+		MaxSymbolWinNum: maxSymbolWinNum,
 	}
 
 	for _, v := range lst {
-		ssrtp.MapSymbols[v] = NewSymbolRTP(v, maxSymbolNum)
+		ssrtp.MapSymbols[v] = NewSymbolRTP(v, maxSymbolWinNum)
 	}
 
 	return ssrtp
