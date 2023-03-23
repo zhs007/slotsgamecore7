d9f88bd7013ee70011c90dc877a3f24dbb8aeb4b
zhs007 <sssxueren@gmail.com>
2023-03-23 16:22:37 +0800

    Updated mathtoolset.GenMath

--- mathtoolset/genmath.go
+++ mathtoolset/genmath.go
@@ -1,7 +1,10 @@
 package mathtoolset
 
 import (
+	"fmt"
+
 	"github.com/zhs007/goutils"
+	sgc7game "github.com/zhs007/slotsgamecore7/game"
 	"go.uber.org/zap"
 )
 
@@ -9,6 +12,14 @@ func GenMath(fn string) error {
 	cfg, err := LoadConfig(fn)
 	if err != nil {
 		goutils.Error("GenMath:LoadConfig",
+			zap.Error(err))
+
+		return err
+	}
+
+	paytables, err := sgc7game.LoadPaytablesFromExcel(cfg.Paytables)
+	if err != nil {
+		goutils.Error("GenMath:LoadPaytablesFromExcel",
 			zap.Error(err))
 
 		return err
@@ -45,6 +56,60 @@ func GenMath(fn string) error {
 			goutils.Error("GenMath:Eval",
 				zap.Float64("cur-rtp", out.Value().(float64)),
 				zap.Error(ErrInvalidTargetRTP))
+
+			return err
+		}
+	} else if cfg.Type == "calcRTPWithReelsState" {
+		mgrGenMath := NewGamMathMgr()
+
+		script, err := NewScriptCore(mgrGenMath)
+		if err != nil {
+			goutils.Error("GenMath:NewScriptCore",
+				zap.Error(err))
+
+			return err
+		}
+
+		err = script.Compile(cfg.Code)
+		if err != nil {
+			goutils.Error("GenMath:Compile",
+				zap.Error(err))
+
+			return err
+		}
+
+		out, err := script.Eval()
+		if err != nil {
+			goutils.Error("GenMath:Eval",
+				zap.Error(err))
+
+			return err
+		}
+
+		fmt.Printf("The RTP is %v", out.Value().(float64))
+	} else if cfg.Type == "genReels" {
+		rss, err := LoadReelsStats(cfg.GenReelsConfig.ReelsStatsFilename)
+		if err != nil {
+			goutils.Error("GenMath:LoadReelsStats",
+				zap.Error(err))
+
+			return err
+		}
+
+		mainSymbols := GetSymbols(cfg.GenReelsConfig.MainSymbols, paytables)
+
+		reels, err := GenReelsMainSymbolsDistance(rss, mainSymbols, cfg.GenReelsConfig.Offset, 100)
+		if err != nil {
+			goutils.Error("GenMath:GenReelsMainSymbolsDistance",
+				zap.Error(err))
+
+			return err
+		}
+
+		err = reels.SaveExcel(cfg.GenReelsConfig.ReelsFilename)
+		if err != nil {
+			goutils.Error("GenMath:SaveExcel",
+				zap.Error(err))
 
 			return err
 		}
