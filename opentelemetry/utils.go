b97a3cab7ebb82f842f956a59048cc7601d099fd
zhs007 <sssxueren@gmail.com>
2023-05-16 18:49:51 +0800

    Updated OpenTelemetry

--- opentelemetry/utils.go
+++ opentelemetry/utils.go
@@ -7,11 +7,14 @@ import (
 	"github.com/zhs007/goutils"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 	"go.uber.org/zap"
 )
 
-func ConfigureJaeger(ctx context.Context) (func(), error) {
+func ConfigureJaeger(ctx context.Context, appname string) (func(), error) {
 	if os.Getenv("OTEL_EXPORTER_JAEGER_ENDPOINT") == "" {
 		return nil, nil
 	}
@@ -19,7 +22,7 @@ func ConfigureJaeger(ctx context.Context) (func(), error) {
 	provider := sdktrace.NewTracerProvider()
 	otel.SetTracerProvider(provider)
 
-	exp, err := jaeger.New(jaeger.WithCollectorEndpoint())
+	exporter, err := jaeger.New(jaeger.WithCollectorEndpoint())
 	if err != nil {
 		goutils.Error("ConfigureJaeger",
 			zap.Error(err))
@@ -27,11 +30,16 @@ func ConfigureJaeger(ctx context.Context) (func(), error) {
 		return nil, err
 	}
 
-	bsp := sdktrace.NewBatchSpanProcessor(exp)
-	provider.RegisterSpanProcessor(bsp)
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(resource.NewSchemaless(semconv.ServiceNameKey.String(appname))),
+	)
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
 
 	return func() {
-		if err := provider.Shutdown(ctx); err != nil {
+		if err := tp.Shutdown(ctx); err != nil {
 			goutils.Error("ConfigureJaeger:Shutdown",
 				zap.Error(err))
 		}
