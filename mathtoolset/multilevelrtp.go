ee4b2effe90c3cd1681e921dfd73475ef955ded4
zhs007 <sssxueren@gmail.com>
2023-11-24 11:44:56 +0800

    Fixed a bug in MultiLevelRTPData

--- mathtoolset/multilevelrtp.go
+++ mathtoolset/multilevelrtp.go
@@ -36,46 +36,26 @@ func (rtpdata *MultiLevelRTPData) add(spinnum int, endinglevel int, rtp float64)
 
 func (rtpdata *MultiLevelRTPData) calcMulLevelRTP2(prelevel int, levelRTPs []float64, levelUpProbs []map[int]float64, spinNum int, levelUpAddSpinNum []int,
 	totalSpinNum int, totalRTP float64) float64 {
-	if prelevel >= len(levelRTPs) {
-		prelevel = len(levelRTPs) - 1
-	}
 
 	// 如果最后一次spin了
 	if spinNum == 0 {
 		return 0
 	}
 
-	// 	已经到最高级了
-	if prelevel == len(levelRTPs)-1 {
-		// 如果不能增加次数，则直接返回即可
-		if levelUpAddSpinNum[prelevel] == 0 {
-			rtpdata.add(totalSpinNum+spinNum, prelevel, totalRTP+levelRTPs[prelevel]*float64(spinNum))
-
-			return levelRTPs[prelevel] * float64(spinNum)
-		}
-
-		mapProbs := levelUpProbs[prelevel]
-		x := float64(0)
-		for k, v := range mapProbs {
-			x += float64(k) * v
-		}
-
-		if x >= 1 {
-			goutils.Error("calcMulLevelRTP2",
-				zap.Error(ErrCannotBeConverged))
-
-			return math.NaN()
-		}
-
-		// 否则需要返回无穷级数求和
-		return levelRTPs[prelevel] * float64(spinNum) / (1 - x)
+	if prelevel >= len(levelRTPs) {
+		prelevel = len(levelRTPs) - 1
 	}
 
-	currtp := float64(0)
+	// 	已经到最高级了，这里不考虑最高级依然可以增加次数，所以直接返回即可
+	if prelevel == len(levelRTPs)-1 {
+		rtpdata.add(totalSpinNum+spinNum, prelevel, totalRTP+levelRTPs[prelevel]*float64(spinNum))
+
+		return levelRTPs[prelevel] * float64(spinNum)
+	}
+
+	currtp := levelRTPs[prelevel]
 
 	mapProbs := levelUpProbs[prelevel]
-
-	noupprob := float64(1)
 
 	for k, v := range mapProbs {
 		if k == 0 {
@@ -83,31 +63,23 @@ func (rtpdata *MultiLevelRTPData) calcMulLevelRTP2(prelevel int, levelRTPs []flo
 		}
 
 		addnum := 0
-		for i := 0; i < k; i++ {
+		for i := 1; i < k; i++ {
 			cl := prelevel + i
-			if cl >= len(levelRTPs) {
-				cl = len(levelRTPs) - 1
-			}
 
-			addnum += levelUpAddSpinNum[cl]
+			if cl < len(levelRTPs) {
+				addnum += levelUpAddSpinNum[cl]
+			}
 		}
 
 		if spinNum-1+addnum > 0 {
 			// 考虑升级的情况
 			currtp += rtpdata.calcMulLevelRTP2(prelevel+k, levelRTPs, levelUpProbs, spinNum-1+addnum, levelUpAddSpinNum, totalSpinNum+1, totalRTP+currtp) * v
-
-			noupprob -= v
 		}
 	}
 
+	// 如果最后一次
 	if spinNum == 1 {
-		// 本次不能升级的情况
-		currtp += levelRTPs[prelevel] * noupprob
-
 		rtpdata.add(totalSpinNum+spinNum, prelevel, totalRTP+currtp)
-	} else {
-		// 本次不能升级的情况
-		currtp += rtpdata.calcMulLevelRTP2(prelevel, levelRTPs, levelUpProbs, spinNum-1, levelUpAddSpinNum, totalSpinNum+1, totalRTP+currtp) * noupprob
 	}
 
 	return currtp
