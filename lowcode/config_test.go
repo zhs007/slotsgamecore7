904b9236a0c06794ad0adcca6935492b307b715c
zhs007 <sssxueren@gmail.com>
2024-02-21 16:19:23 +0800

    Removed some files

--- lowcode/config_test.go
+++
@@ -1,9 +0,0 @@
-package lowcode
-
-// func Test_LoadConfig(t *testing.T) {
-// 	cfg, err := LoadConfig("../data/game001/rtp96.yaml")
-// 	assert.NoError(t, err)
-// 	assert.NotNil(t, cfg)
-
-// 	t.Logf("Test_LoadConfig OK")
-// }
--- lowcode/isymboltrigger.go
+++
@@ -1,6 +0,0 @@
-package lowcode
-
-// type ISymbolTrigger interface {
-// 	// CanTrigger -
-// 	CanTrigger(gameProp *GameProperty, gs *sgc7game.GameScene, curpr *sgc7game.PlayResult, stake *sgc7game.Stake, isSaveResult bool, cd IComponentData) (bool, []*sgc7game.Result)
-// }
