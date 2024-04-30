913141bad8721a79179d678dc5f694af15312bc2
zhs007 <sssxueren@gmail.com>
2024-04-30 16:06:53 +0800

    Updated pos

--- lowcode/componentdatapos.go
+++ lowcode/componentdatapos.go
@@ -1,7 +1,21 @@
 package lowcode
 
+import "github.com/zhs007/goutils"
+
 type PosComponentData struct {
 	Pos []int
+}
+
+func (posdata *PosComponentData) MergePosList(pos []int) {
+	for i := 0; i < len(pos)/2; i++ {
+		posdata.Add(pos[i*2], pos[i*2+1])
+	}
+}
+
+func (posdata *PosComponentData) Add(x, y int) {
+	if goutils.IndexOfInt2Slice(posdata.Pos, x, y, 0) < 0 {
+		posdata.Pos = append(posdata.Pos, x, y)
+	}
 }
 
 func (posdata *PosComponentData) Has(target *PosComponentData) bool {
