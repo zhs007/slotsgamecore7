f298afb7fa66878d152943b63861c9eb2ac7d6b2
zhs007 <sssxueren@gmail.com>
2024-09-24 18:52:48 +0800

    Fixed some bugs in slotsgamecore7

--- game/adjacentpay.go
+++ game/adjacentpay.go
@@ -13,8 +13,8 @@ func CalcAdjacentPay(scene *GameScene, pt *PayTables, bet int,
 	for x, arr := range scene.Arr {
 		for y := range arr {
 			if scene0.Arr[x][y] >= 0 && isValidSymbol(scene0.Arr[x][y]) {
-				crx := calcAdjacentPayWithX(scene0, x, y, getSymbol(scene0.Arr[x][y]), pt, bet, isSameSymbol)
-				cry := calcAdjacentPayWithY(scene0, x, y, getSymbol(scene0.Arr[x][y]), pt, bet, isSameSymbol)
+				crx := calcAdjacentPayWithX(scene0, x, y, getSymbol(scene0.Arr[x][y]), pt, bet, isSameSymbol, isWild)
+				cry := calcAdjacentPayWithY(scene0, x, y, getSymbol(scene0.Arr[x][y]), pt, bet, isSameSymbol, isWild)
 
 				if crx != nil {
 					results = append(results, crx)
@@ -47,8 +47,119 @@ func CalcAdjacentPay(scene *GameScene, pt *PayTables, bet int,
 }
 
 // calcAdjacentPayWithX - AdjacentPay
-func calcAdjacentPayWithX(scene *GameScene, x, y int, symbol int, pt *PayTables, bet int, isSameSymbol FuncIsSameSymbol) *Result {
+func calcAdjacentPayWithX(scene *GameScene, x, y int, symbol int, pt *PayTables, bet int, isSameSymbol FuncIsSameSymbol, isWild FuncIsWild) *Result {
 	pos := []int{x, y}
+
+	if isWild(symbol) {
+		wpos := []int{x, y}
+		curs := -1
+		for tx := 1; x+tx < scene.Width; tx++ {
+			if curs == -1 {
+				if isWild(scene.Arr[x+tx][y]) {
+					wpos = append(wpos, x+tx, y)
+					pos = append(pos, x+tx, y)
+				} else {
+					pos = append(pos, x+tx, y)
+
+					curs = scene.Arr[x+tx][y]
+				}
+			} else if isSameSymbol(scene.Arr[x+tx][y], curs) {
+				pos = append(pos, x+tx, y)
+			} else {
+				break
+			}
+		}
+
+		wnums := len(wpos) / 2
+		if wnums > len(pt.MapPay[symbol]) {
+			wnums = len(pt.MapPay[symbol])
+		}
+
+		if curs == -1 {
+			if pt.MapPay[symbol][wnums-1] > 0 {
+				r := &Result{
+					Symbol:     symbol,
+					Type:       RTAdjacentPay,
+					LineIndex:  -1,
+					Pos:        wpos,
+					SymbolNums: wnums,
+					Mul:        pt.MapPay[symbol][wnums-1],
+					CoinWin:    pt.MapPay[symbol][wnums-1],
+					CashWin:    pt.MapPay[symbol][wnums-1] * bet,
+				}
+
+				return r
+			}
+		}
+
+		nums := len(pos) / 2
+
+		if nums > len(pt.MapPay[curs]) {
+			nums = len(pt.MapPay[curs])
+		}
+
+		if pt.MapPay[curs][nums-1] > 0 && pt.MapPay[symbol][wnums-1] > 0 {
+			if pt.MapPay[curs][nums-1] > pt.MapPay[symbol][wnums-1] {
+				r := &Result{
+					Symbol:     curs,
+					Type:       RTAdjacentPay,
+					LineIndex:  -1,
+					Pos:        pos,
+					SymbolNums: nums,
+					Mul:        pt.MapPay[curs][nums-1],
+					CoinWin:    pt.MapPay[curs][nums-1],
+					CashWin:    pt.MapPay[curs][nums-1] * bet,
+				}
+
+				return r
+			}
+
+			r := &Result{
+				Symbol:     symbol,
+				Type:       RTAdjacentPay,
+				LineIndex:  -1,
+				Pos:        wpos,
+				SymbolNums: wnums,
+				Mul:        pt.MapPay[symbol][wnums-1],
+				CoinWin:    pt.MapPay[symbol][wnums-1],
+				CashWin:    pt.MapPay[symbol][wnums-1] * bet,
+			}
+
+			return r
+		}
+
+		if pt.MapPay[symbol][wnums-1] > 0 {
+			r := &Result{
+				Symbol:     symbol,
+				Type:       RTAdjacentPay,
+				LineIndex:  -1,
+				Pos:        wpos,
+				SymbolNums: wnums,
+				Mul:        pt.MapPay[symbol][wnums-1],
+				CoinWin:    pt.MapPay[symbol][wnums-1],
+				CashWin:    pt.MapPay[symbol][wnums-1] * bet,
+			}
+
+			return r
+		}
+
+		if pt.MapPay[curs][nums-1] > 0 {
+			r := &Result{
+				Symbol:     symbol,
+				Type:       RTAdjacentPay,
+				LineIndex:  -1,
+				Pos:        wpos,
+				SymbolNums: wnums,
+				Mul:        pt.MapPay[symbol][wnums-1],
+				CoinWin:    pt.MapPay[symbol][wnums-1],
+				CashWin:    pt.MapPay[symbol][wnums-1] * bet,
+			}
+
+			return r
+		}
+
+		return nil
+	}
 
 	for tx := 1; x+tx < scene.Width; tx++ {
 		if isSameSymbol(scene.Arr[x+tx][y], symbol) {
@@ -83,8 +194,120 @@ func calcAdjacentPayWithX(scene *GameScene, x, y int, symbol int, pt *PayTables,
 }
 
 // calcAdjacentPayWithY - AdjacentPay
-func calcAdjacentPayWithY(scene *GameScene, x, y int, symbol int, pt *PayTables, bet int, isSameSymbol FuncIsSameSymbol) *Result {
+func calcAdjacentPayWithY(scene *GameScene, x, y int, symbol int, pt *PayTables, bet int, isSameSymbol FuncIsSameSymbol, isWild FuncIsWild) *Result {
 	pos := []int{x, y}
+
+	if isWild(symbol) {
+		wpos := []int{x, y}
+		curs := -1
+
+		for ty := 1; y+ty < scene.Height; ty++ {
+			if curs == -1 {
+				if isWild(scene.Arr[x][y+ty]) {
+					wpos = append(wpos, x, y+ty)
+					pos = append(pos, x, y+ty)
+				} else {
+					pos = append(pos, x, y+ty)
+
+					curs = scene.Arr[x][y+ty]
+				}
+			} else if isSameSymbol(scene.Arr[x][y+ty], curs) {
+				pos = append(pos, x, y+ty)
+			} else {
+				break
+			}
+		}
+
+		wnums := len(wpos) / 2
+		if wnums > len(pt.MapPay[symbol]) {
+			wnums = len(pt.MapPay[symbol])
+		}
+
+		if curs == -1 {
+			if pt.MapPay[symbol][wnums-1] > 0 {
+				r := &Result{
+					Symbol:     symbol,
+					Type:       RTAdjacentPay,
+					LineIndex:  -1,
+					Pos:        wpos,
+					SymbolNums: wnums,
+					Mul:        pt.MapPay[symbol][wnums-1],
+					CoinWin:    pt.MapPay[symbol][wnums-1],
+					CashWin:    pt.MapPay[symbol][wnums-1] * bet,
+				}
+
+				return r
+			}
+		}
+
+		nums := len(pos) / 2
+
+		if nums > len(pt.MapPay[curs]) {
+			nums = len(pt.MapPay[curs])
+		}
+
+		if pt.MapPay[curs][nums-1] > 0 && pt.MapPay[symbol][wnums-1] > 0 {
+			if pt.MapPay[curs][nums-1] > pt.MapPay[symbol][wnums-1] {
+				r := &Result{
+					Symbol:     curs,
+					Type:       RTAdjacentPay,
+					LineIndex:  -1,
+					Pos:        pos,
+					SymbolNums: nums,
+					Mul:        pt.MapPay[curs][nums-1],
+					CoinWin:    pt.MapPay[curs][nums-1],
+					CashWin:    pt.MapPay[curs][nums-1] * bet,
+				}
+
+				return r
+			}
+
+			r := &Result{
+				Symbol:     symbol,
+				Type:       RTAdjacentPay,
+				LineIndex:  -1,
+				Pos:        wpos,
+				SymbolNums: wnums,
+				Mul:        pt.MapPay[symbol][wnums-1],
+				CoinWin:    pt.MapPay[symbol][wnums-1],
+				CashWin:    pt.MapPay[symbol][wnums-1] * bet,
+			}
+
+			return r
+		}
+
+		if pt.MapPay[symbol][wnums-1] > 0 {
+			r := &Result{
+				Symbol:     symbol,
+				Type:       RTAdjacentPay,
+				LineIndex:  -1,
+				Pos:        wpos,
+				SymbolNums: wnums,
+				Mul:        pt.MapPay[symbol][wnums-1],
+				CoinWin:    pt.MapPay[symbol][wnums-1],
+				CashWin:    pt.MapPay[symbol][wnums-1] * bet,
+			}
+
+			return r
+		}
+
+		if pt.MapPay[curs][nums-1] > 0 {
+			r := &Result{
+				Symbol:     symbol,
+				Type:       RTAdjacentPay,
+				LineIndex:  -1,
+				Pos:        wpos,
+				SymbolNums: wnums,
+				Mul:        pt.MapPay[symbol][wnums-1],
+				CoinWin:    pt.MapPay[symbol][wnums-1],
+				CashWin:    pt.MapPay[symbol][wnums-1] * bet,
+			}
+
+			return r
+		}
+
+		return nil
+	}
 
 	for ty := 1; y+ty < scene.Height; ty++ {
 		if isSameSymbol(scene.Arr[x][y+ty], symbol) {
--- ver/version.go
+++ ver/version.go
@@ -1,4 +1,4 @@
 package sgc7ver
 
 // Version -
-const Version = "v0.13.350"
+const Version = "v0.13.351"
