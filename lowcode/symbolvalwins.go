c93b09f507b14a8b8f042968f8a10bb8fbe5fd86
zhs007 <sssxueren@gmail.com>
2024-11-18 18:10:55 +0800

    Added OptIntVal in symbolValsWins

--- lowcode/symbolvalwins.go
+++ lowcode/symbolvalwins.go
@@ -36,14 +36,12 @@ func parseSymbolValWinsType(strType string) SymbolValWinsType {
 const (
 	SVWDVWins      string = "wins"      // 中奖的数值，线注的倍数
 	SVWDVSymbolNum string = "symbolNum" // 符号数量
-	// SVWDVCollectorNum string = "collectorNum" // 收集器数量
 )
 
 type SymbolValWinsData struct {
 	BasicComponentData
 	SymbolNum int
 	Wins      int
-	// CollectorNum int
 }
 
 // OnNewGame -
@@ -53,15 +51,9 @@ func (symbolValWinsData *SymbolValWinsData) OnNewGame(gameProp *GameProperty, co
 
 // onNewStep -
 func (symbolValWinsData *SymbolValWinsData) onNewStep() {
-	// symbolValWinsData.BasicComponentData.OnNewStep(gameProp, component)
-
 	symbolValWinsData.UsedResults = nil
 	symbolValWinsData.SymbolNum = 0
 	symbolValWinsData.Wins = 0
-
-	// symbolValWinsData.SymbolNum = 0
-	// symbolValWinsData.Wins = 0
-	// symbolValWinsData.CollectorNum = 0
 }
 
 // Clone
@@ -84,7 +76,6 @@ func (symbolValWinsData *SymbolValWinsData) BuildPBComponentData() proto.Message
 	if !gIsReleaseMode {
 		pbcd.SymbolNum = int32(symbolValWinsData.SymbolNum)
 		pbcd.Wins = int32(symbolValWinsData.Wins)
-		// pbcd.CollectorNum = int32(symbolValWinsData.CollectorNum)
 	}
 
 	return pbcd
@@ -102,15 +93,6 @@ func (symbolValWinsData *SymbolValWinsData) GetValEx(key string, getType GetComp
 
 	return 0, false
 }
-
-// // SetVal -
-// func (symbolValWinsData *SymbolValWinsData) SetVal(key string, val int) {
-// 	if key == CVWins {
-// 		symbolValWinsData.Wins = val
-// 	} else if key == CVSymbolNum {
-// 		symbolValWinsData.SymbolNum = val
-// 	}
-// }
 
 // SymbolValWinsConfig - configuration for SymbolValWins
 type SymbolValWinsConfig struct {
@@ -189,37 +171,8 @@ func (symbolValWins *SymbolValWins) InitEx(cfg any, pool *GamePropertyPool) erro
 func (symbolValWins *SymbolValWins) OnPlayGame(gameProp *GameProperty, curpr *sgc7game.PlayResult, gp *GameParams, plugin sgc7plugin.IPlugin,
 	cmd string, param string, ps sgc7game.IPlayerState, stake *sgc7game.Stake, prs []*sgc7game.PlayResult, icd IComponentData) (string, error) {
 
-	// symbolValWins.onPlayGame(gameProp, curpr, gp, plugin, cmd, param, ps, stake, prs)
-
 	svwd := icd.(*SymbolValWinsData)
 	svwd.onNewStep()
-
-	// gs := symbolValWins.GetTargetScene3(gameProp, curpr, prs, &svwd.BasicComponentData, symbolValWins.Name, "", 0)
-	// isTrigger := true
-	// symbolnum := 0
-
-	// svwd.SymbolNum = 0
-	// svwd.Wins = 0
-	// svwd.CollectorNum = 0
-
-	// if symbolValWins.TriggerSymbolCode >= 0 {
-	// 	isTrigger = false
-
-	// 	// if symbolValWins.Config.Type == WinTypeCountScatter {
-	// 	ret := sgc7game.CalcScatterEx(gs, symbolValWins.TriggerSymbolCode, symbolValWins.Config.MinNum, func(scatter int, cursymbol int) bool {
-	// 		return cursymbol == scatter
-	// 	})
-
-	// 	if ret != nil {
-	// 		isTrigger = true
-
-	// 		symbolnum = ret.SymbolNums
-	// 	}
-	// 	// }
-	// }
-
-	// if isTrigger {
-	// svwd.CollectorNum = symbolnum
 
 	var gs *sgc7game.GameScene
 	os := symbolValWins.GetTargetOtherScene3(gameProp, curpr, prs, 0)
@@ -272,7 +225,6 @@ func (symbolValWins *SymbolValWins) OnPlayGame(gameProp *GameProperty, curpr *sg
 				newpos = append(newpos, pos...)
 
 				ret := &sgc7game.Result{
-					// Symbol:     gs.Arr[newpos[0]][newpos[1]],
 					Type:       sgc7game.RTCoins,
 					LineIndex:  -1,
 					Pos:        newpos,
@@ -293,33 +245,11 @@ func (symbolValWins *SymbolValWins) OnPlayGame(gameProp *GameProperty, curpr *sg
 				symbolValWins.AddResult(curpr, ret, &svwd.BasicComponentData)
 			}
 
-			// ret := &sgc7game.Result{
-			// 	Symbol:     -1, //gs.Arr[pos[0]][pos[1]],
-			// 	Type:       sgc7game.RTSymbolVal,
-			// 	LineIndex:  -1,
-			// 	Pos:        pos,
-			// 	SymbolNums: len(pos) / 2,
-			// 	Mul:        mul,
-			// }
-
-			// bet := gameProp.GetBet2(stake, symbolValWins.Config.BetType)
-
-			// othermul := symbolValWins.GetWinMulti(&svwd.BasicComponentData)
-
-			// ret.CoinWin = totalvals * mul * othermul
-			// ret.CashWin = ret.CoinWin * bet
-			// ret.OtherMul = othermul
-
-			// svwd.Wins = ret.CoinWin
-
-			// symbolValWins.AddResult(curpr, ret, &svwd.BasicComponentData)
-
 			nc := symbolValWins.onStepEnd(gameProp, curpr, gp, "")
 
 			return nc, nil
 		}
 	}
-	// }
 
 	nc := symbolValWins.onStepEnd(gameProp, curpr, gp, "")
 
@@ -337,60 +267,6 @@ func (symbolValWins *SymbolValWins) OnAsciiGame(gameProp *GameProperty, pr *sgc7
 	return nil
 }
 
-// // OnStatsWithPB -
-// func (symbolValWins *SymbolValWins) OnStatsWithPB(feature *sgc7stats.Feature, pbComponentData proto.Message, pr *sgc7game.PlayResult) (int64, error) {
-// 	return 0, nil
-// 	// pbcd, isok := pbComponentData.(*sgc7pb.SymbolValWinsData)
-// 	// if !isok {
-// 	// 	goutils.Error("SymbolValWins.OnStatsWithPB",
-// 	// 		goutils.Err(ErrIvalidProto))
-
-// 	// 	return 0, ErrIvalidProto
-// 	// }
-
-// 	// return symbolValWins.OnStatsWithPBBasicComponentData(feature, pbcd.BasicComponentData, pr), nil
-// }
-
-// // OnStats
-// func (symbolValWins *SymbolValWins) OnStats(feature *sgc7stats.Feature, stake *sgc7game.Stake, lst []*sgc7game.PlayResult) (bool, int64, int64) {
-// 	return false, 0, 0
-// 	// wins := int64(0)
-// 	// isTrigger := false
-
-// 	// for _, v := range lst {
-// 	// 	gp, isok := v.CurGameModParams.(*GameParams)
-// 	// 	if isok {
-// 	// 		curComponent, isok := gp.MapComponentMsgs[symbolValWins.Name]
-// 	// 		if isok {
-// 	// 			curwins, err := symbolValWins.OnStatsWithPB(feature, curComponent, v)
-// 	// 			if err != nil {
-// 	// 				goutils.Error("SymbolValWins.OnStats",
-// 	// 					goutils.Err(err))
-
-// 	// 				continue
-// 	// 			}
-
-// 	// 			isTrigger = true
-// 	// 			wins += curwins
-// 	// 		}
-// 	// 	}
-// 	// }
-
-// 	// feature.CurWins.AddWin(int(wins) * 100 / int(stake.CashBet))
-
-// 	// if feature.Parent != nil {
-// 	// 	totalwins := int64(0)
-
-// 	// 	for _, v := range lst {
-// 	// 		totalwins += v.CashWin
-// 	// 	}
-
-// 	// 	feature.AllWins.AddWin(int(totalwins) * 100 / int(stake.CashBet))
-// 	// }
-
-// 	// return isTrigger, stake.CashBet, wins
-// }
-
 // NewComponentData -
 func (symbolValWins *SymbolValWins) NewComponentData() IComponentData {
 	return &SymbolValWinsData{}
@@ -398,7 +274,7 @@ func (symbolValWins *SymbolValWins) NewComponentData() IComponentData {
 
 // NewStats2 -
 func (symbolValWins *SymbolValWins) NewStats2(parent string) *stats2.Feature {
-	return stats2.NewFeature(parent, stats2.Options{stats2.OptWins})
+	return stats2.NewFeature(parent, stats2.Options{stats2.OptWins, stats2.OptIntVal})
 }
 
 // OnStats2
@@ -408,6 +284,10 @@ func (symbolValWins *SymbolValWins) OnStats2(icd IComponentData, s2 *stats2.Cach
 	svwd := icd.(*SymbolValWinsData)
 
 	s2.ProcStatsWins(symbolValWins.Name, int64(svwd.Wins))
+
+	multi := symbolValWins.GetWinMulti(&svwd.BasicComponentData)
+
+	s2.ProcStatsIntVal(symbolValWins.GetName(), multi)
 }
 
 func (symbolValWins *SymbolValWins) GetWinMulti(basicCD *BasicComponentData) int {
@@ -439,8 +319,6 @@ func (jcfg *jsonSymbolValWins) build() *SymbolValWinsConfig {
 		Symbols:       jcfg.Symbols,
 		StrType:       jcfg.Type,
 	}
-
-	// cfg.UseSceneV3 = true
 
 	return cfg
 }
